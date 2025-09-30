daf49908f8e2c72a3fc6ddfcde9e22279721eead
Tommy Fritz <tommy@covidence.org>
2025-09-30 16:33:24 +1300

    update for Rails 8

---
+++ lib/representative_view/action_pack_8_handler.rb
@@ -0,0 +1,19 @@
+require 'representative_view/view_helpers'
+
+module RepresentativeView
+
+  class ActionPack8Handler
+
+    def self.call(template, source)
+      <<-RUBY
+      representative_view(formats.first) do |r|
+        #{source}
+      end
+      RUBY
+    end
+
+  end
+
+end
+
+ActionView::Template.register_template_handler(:rep, RepresentativeView::ActionPack8Handler)
